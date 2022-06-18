bc20d5f4d0708ad66bbd2b37e314925560632af7
EC2 Default User <ec2-user@ip-172-31-21-248.ec2.internal>
2022-06-18 05:49:21 +0000

    __

--- db/migrate/20220618043817_devise_create_admins.rb
+++ db/migrate/20220618043817_devise_create_admins.rb
@@ -36,9 +36,6 @@ class DeviseCreateAdmins < ActiveRecord::Migration[6.1]
       t.timestamps null: false
     end
 
-　　t.string :email
-    t.string :encrypted_password
-    
     add_index :admins, :email,                unique: true
     add_index :admins, :reset_password_token, unique: true
     # add_index :admins, :confirmation_token,   unique: true
