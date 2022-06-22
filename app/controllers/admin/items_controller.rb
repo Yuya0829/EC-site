17e756f504896a500a953793ec0f87c4d65f5b89
EC2 Default User <ec2-user@ip-172-31-21-248.ec2.internal>
2022-06-22 12:35:56 +0000

    3.1.7

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -21,10 +21,24 @@ class Admin::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @tax_out = ((@item.price) / 1.10).to_i
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:success] = "Item was successfully updated."
+      redirect_to admin_items_path
+    else
+      render :edit
+    end
+  end
+
 
   def item_params
     params.require(:item).permit(:genre_id, :name, :image, :introduction, :price)
