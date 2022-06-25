2b2c636c880115b21c2011b7adce4230aeb9db1d
EC2 Default User <ec2-user@ip-172-31-21-248.ec2.internal>
2022-06-25 12:01:51 +0000

    h&f fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'homes/top'
-    get "home/about" => "homes#about", as: "about"
+  scope module: :public do
+    get '/' => 'homes#top'
+    get "about" => "homes#about", as: "about"
   end
 
   namespace :admin do
+    get '/' => 'homes#top'
     get 'items/new'
     get 'items/index'
     get 'items/show'
     get 'items/edit'
+    resources :homes, :genres, :items
   end
+
 
 # 顧客用
 devise_for :customers,skip: [:passwords], controllers: {
@@ -22,13 +25,5 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin,skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
-
-namespace :admin do
-  resources :genres, :items
-end
-
-namespace :public do
-  resources :homes
-end
 
 end
