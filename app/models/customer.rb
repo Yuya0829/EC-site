6a03bfde8424ed1caa79b24d65c5b0fce26f85e5
EC2 Default User <ec2-user@ip-172-31-21-248.ec2.internal>
2022-06-26 13:58:07 +0000

    3.2.4

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,11 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+   validates :last_name, :first_name, :last_name_kana, :first_name_kana, presence: true
+   validates :email, :postal_code, :address, :telephone_number, presence: true
+   validates :encrypted_password, length: { minimum: 6 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
