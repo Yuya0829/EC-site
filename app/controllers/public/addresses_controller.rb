8f51fcf641e6894ebea394a3c9ad97f73ce416a7
EC2 Default User <ec2-user@ip-172-31-21-248.ec2.internal>
2022-07-01 13:04:52 +0000

    3.2.7-9 on the way

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,24 @@
 class Public::AddressesController < ApplicationController
+
+  def index
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def addresses_params
+    params.require(:address).permit(:name, :postal_code, :address)
+  end
+
 end
