b49a46566c627fcedb483659513802e3625bba68
EC2 Default User <ec2-user@ip-172-31-21-248.ec2.internal>
2022-07-12 05:21:46 +0000

    3.2.14

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,21 +10,21 @@ class Public::OrdersController < ApplicationController
     @cart_items = current_customer.cart_items
     @total = 0
     @order = Order.new(order_params)
+    @order.shipping_cost = 800
     @select_address = params[:select_address]
      if @select_address == "0"
-        @address = Customer.find_by(id: current_customer.id)
-        @postal_code = @address.postal_code
-        @current_customer_address = @address.address
-        @name =  @address.last_name + @address.first_name
+        @order.postal_code = current_customer.postal_code
+        @order.address = current_customer.address
+        @order.name =  current_customer.last_name + current_customer.first_name
      elsif @select_address == "1"
         @address = Address.find(params[:address_id])
-        @postal_code = @address.postal_code
-        @current_customer_address = @address.address
-        @name = @address.name
+        @order.postal_code = @address.postal_code
+        @order.current_customer_address = @address.address
+        @order.name = @address.name
      else
-        @postal_code = params[:order][:postal_code]
-        @current_customer_address = params[:order][:address]
-        @name = params[:order][:name]
+        @order.postal_code = params[:order][:postal_code]
+        @order.address = params[:order][:address]
+        @order.name = params[:order][:name]
      end
     #binding.pry
   end
@@ -36,10 +36,22 @@ class Public::OrdersController < ApplicationController
      @order = Order.new(order_params)
     if @order.save
       flash[:notice] = "Order was successfully created."
-      redirect_to new_order_path
+      @cart_items = current_customer.cart_items
+      @cart_items.each do |cart_item|
+        order_detail = OrderDetail.new
+        order_detail.item_id = cart_item.item.id
+        order_detail.order_id = @order.id
+        order_detail.price = cart_item.item.with_tax_price
+        order_detail.amount = cart_item.amount
+        order_detail.making_status = 0
+        order_detail.save
+      end
+      @cart_items.destroy_all
+      redirect_to orders_compleate_path
     else
       render :new
     end
+
   end
 
   def index
@@ -52,7 +64,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :total_payment, :shipping_cost, :customer_id, :status)
   end
 
 end
