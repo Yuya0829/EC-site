439412ae7212c00a61fa323953e491cdb6265af8
EC2 Default User <ec2-user@ip-172-31-21-248.ec2.internal>
2022-07-13 13:07:01 +0000

    3.1.12

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,28 @@
 class Admin::OrdersController < ApplicationController
+
+  def show
+     @order = Order.find(params[:id])
+     @total = 0
+     @order.order_details.each do |order_detail|
+       @total += order_detail.subtotal
+     end
+     @order.shipping_cost = 800
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      flash[:notice] = "Order was successfully updated."
+      redirect_to admin_order_path(@order.id)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :total_payment, :shipping_cost, :customer_id, :status)
+  end
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,9 +1,11 @@
 class OrderDetail < ApplicationRecord
     belongs_to :order
     belongs_to :item
-    
+
+    enum making_status: { not_startable: 0, waiting_for_production: 1,  in_production: 2, production_completed:3 }
+
   def subtotal
     price * amount
   end
-  
+
 end
